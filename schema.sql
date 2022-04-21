e841d3be9f90fe0948b63dfa67beb1b852c9b641
KathleenYager <kyager@ccdocle.org>
2022-04-20 21:53:01 -0400

    adding retirement_info.csv and queries.sql files

--- schema.sql
+++ schema.sql
@@ -46,7 +46,7 @@ CREATE TABLE dept_emp(
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no,dept_no)
 );
 
 -- Creating Titles table
@@ -56,7 +56,7 @@ CREATE TABLE titles(
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
 SELECT * FROM departments;
